67278aa734f10d1d1bf8c82d6b1ab8d110e9bd2d
Coolertyo4 <lm.pramer@gmail.com>
2022-03-09 13:16:53 +0100

    physics

---
+++ physics/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <log.h>
+
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/opencl.h>
+#endif
+
+
+int main(int argc, char* argv[]) {
+
+}
